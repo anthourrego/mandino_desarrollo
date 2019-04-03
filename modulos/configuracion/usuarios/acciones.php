9200928ebb3a72619bb7dde3fd33355ec84a7fb1
anthourrego <antho.120@hotmail.com>
2019-04-03 15:41:09 -0500

    Editar Perfil
    Se finaliza la parte de editar perfil por parte del usuario

--- modulos/configuracion/usuarios/acciones.php
+++ modulos/configuracion/usuarios/acciones.php
@@ -15,6 +15,58 @@
   require_once($ruta_raiz . "clases/SessionActiva.php");
   require_once($ruta_raiz . "clases/funciones_generales.php");
 
+  function actualizarTema(){
+    $respuesta = "";
+    $db = new Bd();
+    $db->conectar();
+
+    $db->sentencia("UPDATE usuarios SET fk_mt = :fk_mt WHERE u_id = :u_id", array(":fk_mt" => $_POST['radio-tema'], ":u_id" => $_POST['id_usuario']));
+
+    $sql_tema = $db->consulta("SELECT * FROM mandino_temas WHERE mt_id = :mt_id", array(":mt_id" => $_POST['radio-tema']));
+
+    if ($sql_tema['cantidad_registros'] == 1) {
+      
+      $session = new Session();
+      $session->setCampo('usuario', 'navbar', $sql_tema[0]['mt_navbar']);
+      $session->setCampo('usuario', 'logo_navbar', $sql_tema[0]['mt_logo_navbar']);
+
+      $respuesta = "Ok";
+    }else{
+      $respuesta = "No se ha actualizado el tema";
+    }
+
+    $db->desconectar();
+
+    return $respuesta;
+  }
+
+  function actualizarPass(){
+    $db = new Bd();
+    $db->conectar();
+    $respuesta = "";
+
+    $pass = encriptarPass($_POST['pass_actual']);
+    $newPass = encriptarPass($_POST['pass_nuevo']);
+    $renewPass = encriptarPass($_POST['pass_renuevo']);
+
+    $validarPass = $db->consulta("SELECT * FROM usuarios WHERE u_id = :u_id AND u_password = :u_password", array(":u_password" => $pass, ":u_id" => $_POST['id_usuario']));
+
+    if ($validarPass['cantidad_registros'] == 1) {
+      if ($newPass == $renewPass) {
+        $db->sentencia("UPDATE usuarios SET u_password = :u_password WHERE u_id = :u_id", array(":u_password" => $newPass, ":u_id" => $_POST['id_usuario']));
+        $respuesta = "Ok";
+      }else{
+        $respuesta = "Las contraseñas no coinciden";
+      }
+    }else{
+      $respuesta = "La contraseña actual es incorrecta";
+    }
+
+    $db->desconectar();
+
+    return $respuesta;
+  }
+
   function datosUsuario(){
   	$db = new Bd();
   	$db->conectar();
--- modulos/configuracion/usuarios/editar_perfil.php
+++ modulos/configuracion/usuarios/editar_perfil.php
@@ -14,9 +14,45 @@
   include_once($ruta_raiz . 'clases/sessionActiva.php');
   include_once($ruta_raiz . 'clases/Conectar.php');
 
+  $session = new Session();
+
   $usuario = $session->get("usuario");
 
   $lib = new Libreria;
+
+  $db = new Bd();
+  $db->conectar();
+  $tema = "";
+
+  $sql_tema = $db->consulta("SELECT * FROM mandino_temas LEFT JOIN usuarios ON usuarios.fk_mt = mandino_temas.mt_id ORDER BY mt_id ASC");
+
+  for ($i=0; $i < $sql_tema['cantidad_registros']; $i++) { 
+    if($sql_tema[$i]['u_id'] == $usuario['id']){
+      $tema .= '<div class="custom-control custom-radio mb-3">
+                <input type="radio" class="custom-control-input" value="' . $sql_tema[$i]['mt_id'] . '" id="tema-' . $sql_tema[$i]['mt_id'] . '" name="radio-tema" required checked>
+                <label class="custom-control-label w-100" for="tema-' . $sql_tema[$i]['mt_id'] . '">
+                  <nav class="navbar ' . $sql_tema[$i]['mt_navbar'] . '">
+                    <div class="navbar-brand">
+                      <img src="' . $ruta_raiz . 'img/' . $sql_tema[$i]['mt_logo_navbar'] . '" class="w-100" alt="">
+                    </div>
+                  </nav>
+                </label>
+              </div>';
+    }else{
+     $tema .= '<div class="custom-control custom-radio mb-3">
+                <input type="radio" value="' . $sql_tema[$i]['mt_id'] . '" class="custom-control-input" id="tema-' . $sql_tema[$i]['mt_id'] . '" name="radio-tema" required>
+                <label class="custom-control-label w-100" for="tema-' . $sql_tema[$i]['mt_id'] . '">
+                  <nav class="navbar ' . $sql_tema[$i]['mt_navbar'] . '">
+                    <div class="navbar-brand">
+                      <img src="' . $ruta_raiz . 'img/' . $sql_tema[$i]['mt_logo_navbar'] . '" class="w-100" alt="">
+                    </div>
+                  </nav>
+                </label>
+              </div>';
+    }
+  }
+
+  $db->desconectar();
 
 ?>
 
@@ -26,11 +62,14 @@
   <title></title>
   <?php  
     echo $lib->jquery();
+    echo $lib->mandino();
     echo $lib->bootstrap();
     echo $lib->fontAwesome();
+    echo $lib->alertify();
+    echo $lib->jqueryValidate();
   ?>
 </head>
-<body>
+<body class="bg-white">
   <!-- Contenido -->
   <div class="container mt-4">
     <div class="row">
@@ -46,66 +85,89 @@
         <div class="tab-content" id="nav-tabContent">
           <div class="tab-pane fade show active" id="pefil" role="tabpanel" aria-labelledby="list-home-list">
             <div class="row d-flex align-items-center">
-              <div class="col-5">
-                Nro Documento:
+              <div class="col-12">
+                <b>Nro Documento:</b>
               </div>
-              <div class="col-7">
+              <div class="col-12">
                 <span id="nro_documento"></span>
               </div>
             </div>
             <hr>
             <div class="row">
-              <div class="col-5">
-                Usuario:
+              <div class="col-12">
+                <b>Usuario:</b>
               </div>
-              <div class="col-7">
+              <div class="col-12">
                 <span id="usuario"></span>
               </div>
             </div>
             <hr>
             <div class="row">
-              <div class="col-5">
-                Nombre:
+              <div class="col-12">
+                <b>Nombre:</b>
               </div>
-              <div class="col-7">
+              <div class="col-12">
                 <span id="nombre"></span>
               </div>
             </div>
             <hr>
             <div class="row">
-              <div class="col-5">
-                Correo Corportativo:
-              </div>
-              <div class="col-7">
+              <div class="col-12">
+                <b>Correo Corportativo:</b>
                 <span id="correo_corporativo"></span>
+              </div>
+              <div class="col-12">
               </div>
             </div>
             <hr>
             <div class="row">
-              <div class="col-5">
-                Correo Personal:
+              <div class="col-12">
+                <b>Correo Personal:</b>
               </div>
-              <div class="col-7">
+              <div class="col-12">
                 <span id="correo_personal"></span>
               </div>
             </div>
             <hr>
             <div class="row">
-              <div class="col-5">
-                Teléfono:
+              <div class="col-12">
+                <b>Teléfono:</b>
               </div>
-              <div class="col-7">
+              <div class="col-12">
                 <span id="telefono"></span>
               </div>
             </div>
           </div>
           <div class="tab-pane fade" id="cambio-password" role="tabpanel" aria-labelledby="list-profile-list">
-            <form>
-              
+            <form id="formPass" class="needs-validation" novalidate autocomplete="nope">
+              <input type="hidden" id="id_usuario" name="id_usuario" value="">
+              <input type="hidden" name="accion" value="actualizarPass">
+              <div class="form-group">
+                <label>Contraseña Actual *</label>
+                <input class="form-control" type="password" id="pass_actual" name="pass_actual" required>
+              </div>
+              <div class="form-group">
+                <label>Contraseña Nueva *</label>
+                <input class="form-control" type="password" id="pass_nuevo" name="pass_nuevo" required>
+              </div>
+              <div class="form-group">
+                <label>Repetir Contraseña Nueva *</label>
+                <input class="form-control" type="password" id="pass_renuevo" name="pass_renuevo" required>
+              </div>
+              <div class="text-center">
+                <button class="btn btn-success" type="submit"><i class="fas fa-save"></i> Actualizar Contraseña</button>
+              </div>
             </form>
           </div>
           <div class="tab-pane fade" id="temas" role="tabpanel" aria-labelledby="list-messages-list">
-            Temas
+            <form id="formTema">
+              <input type="hidden" id="id_usuario_tema" name="id_usuario" value="">
+              <input type="hidden" name="accion" value="actualizarTema">
+              <?php echo($tema); ?>
+              <div class="text-center">
+                <button class="btn btn-success" type="submit"><i class="far fa-save"></i> Guardar</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
@@ -130,31 +192,91 @@
         $("#correo_corporativo").html(data.u_correo_corporativo);
         $("#correo_personal").html(data.u_correo_personal);
         $("#telefono").html(data.u_telefono);
-
+        $("#id_usuario").val(data.u_id);
+        $("#id_usuario_tema").val(data.u_id);
       },
       error: function(){
         top.alertify.error("No han podido traer los datos usuarios");
       }
     });
     
-  });
+    $("#formPass").validate({
+      debug: false,
+      rules:{
+        pass_actual: "required",
+        pass_nuevo: "required",
+        pass_renuevo: {
+          required: true,
+          equalTo: "#pass_nuevo"
+        }
+      },
+      errorElement: 'span',
+      errorPlacement: function (error, element) {
+        error.addClass('invalid-feedback');
+        element.closest('.form-group').append(error);
+      },
+      highlight: function (element, errorClass, validClass) {
+        $(element).addClass('is-invalid');
+        $(element).removeClass('is-valid');
+      },
+      unhighlight: function (element, errorClass, validClass) {
+        $(element).removeClass('is-invalid');
+        $(element).addClass('is-valid');
+      }
+    });
 
-  (function() {
-    'use strict';
-      window.addEventListener('load', function() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-          form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-              event.preventDefault();
-              event.stopPropagation();
+    $("#formPass").submit(function(event){
+      event.preventDefault();
+      if ($("#formPass").valid()) {
+        $.ajax({
+          url: 'acciones',
+          type: 'POST',
+          cache: false,
+          contentType: false,
+          processData: false,
+          data: new FormData(this),
+          success: function(data){
+            if (data == "Ok") {
+              alertify.success("Se ha actualizado correctamente");
+              $("#formPass")[0].reset();
+            }else{
+              alertify.error(data);
             }
-            form.classList.add('was-validated');
-          }, false);
+          },
+          error: function(){
+            alertify.error("Error al cambiar la contraseña");
+          }
         });
-      }, false);
-    })();
+      }
+    });
+
+    $("#formTema").submit(function(event){
+      event.preventDefault();
+      $.ajax({
+        url: 'acciones',
+        type: 'POST',
+        cache: false,
+        contentType: false,
+        processData: false,
+        data: new FormData(this),
+        success: function(data){
+          if (data == "Ok") {
+            alertify.success("Tema actualizado");
+            setTimeout(function() {
+              top.location.reload();
+            }, 1000);
+
+          }else{
+            alertify.error(data);
+          }
+        },
+        error: function(){
+          alertify.error("No ha actualizado el tema");
+        }
+      });
+      
+    });
+
+  });
 </script>
 </html>
