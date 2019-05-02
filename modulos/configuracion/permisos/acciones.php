19b0a9bf295d9c57f62f0e2a6278e0aafb5c475f
Anthony Smidh Urrego Pineda <antho.120@hotmail.com>
2019-05-01 19:33:49 -0500

    Cambio de posicon

--- modulos/configuracion/permisos/acciones.php
+++ modulos/configuracion/permisos/acciones.php
@@ -11,8 +11,8 @@
     $max_salida--;
   }
 
-  require_once($ruta_raiz . "clases/Conectar.php");
   require_once($ruta_raiz . "clases/SessionActiva.php");
+  require_once($ruta_raiz . "clases/Conectar.php");
   require_once($ruta_raiz . "clases/funciones_generales.php");
   require_once($ruta_raiz . "clases/Upload.php");
 
